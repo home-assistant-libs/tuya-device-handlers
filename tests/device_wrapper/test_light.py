a72c6c9acbc1c8875cf2316d80e995f6f48a3464
epenet <6771947+epenet@users.noreply.github.com>
2026-03-10 09:30:07 +0100

    Improve coverage

--- tests/device_wrapper/test_light.py
+++ tests/device_wrapper/test_light.py
@@ -187,27 +187,31 @@ def test_read_device_status(
 
 
 @pytest.mark.parametrize(
-    ("wrapper_type", "dpcode", "action", "expected"),
+    ("sample", "wrapper_type", "dpcode", "action", "expected"),
     [
         (
+            "default",
             BrightnessWrapper,
             "bright_value",
             255,
             [{"code": "bright_value", "value": 1000}],
         ),
         (
+            "default",
             BrightnessWrapper,
             "bright_value",
             126,
             [{"code": "bright_value", "value": 499}],
         ),
         (
+            "default",
             BrightnessWrapper,
             "bright_value",
             0,
             [{"code": "bright_value", "value": 10}],
         ),
         (
+            "default",
             ColorDataWrapper,
             "colour_data",
             (228.6350974930362, 393.3070866141732, 1002.9330708661417),
@@ -219,26 +223,51 @@ def test_read_device_status(
             ],
         ),
         (
+            "default",
             ColorTempWrapper,
             "temp_value",
             2000,
             [{"code": "temp_value", "value": 0}],
         ),
         (
+            "default",
             ColorTempWrapper,
             "temp_value",
             3059,
             [{"code": "temp_value", "value": 500}],
         ),
         (
+            "default",
             ColorTempWrapper,
             "temp_value",
             6500,
             [{"code": "temp_value", "value": 1000}],
         ),
+        (
+            "extended_brightness",
+            BrightnessWrapper,
+            "bright_value_1",
+            255,
+            [{"code": "bright_value_1", "value": 1000}],
+        ),
+        (
+            "extended_brightness",
+            BrightnessWrapper,
+            "bright_value_1",
+            126,
+            [{"code": "bright_value_1", "value": 499}],
+        ),
+        (
+            "extended_brightness",
+            BrightnessWrapper,
+            "bright_value_1",
+            0,
+            [{"code": "bright_value_1", "value": 10}],
+        ),
     ],
 )
 def test_light_action_command(
+    sample: str,
     wrapper_type: type[DPCodeTypeInformationWrapper[Any, Any]],
     dpcode: str,
     action: str,
@@ -246,6 +275,30 @@ def test_light_action_command(
     mock_device: CustomerDevice,
 ) -> None:
     """Test get_update_commands."""
+    if sample == "default":
+        _inject_default_light(mock_device)
+    elif sample == "extended_brightness":
+        inject_dpcode(
+            mock_device,
+            "bright_value_1",
+            1000,
+            dptype="Integer",
+            values='{"min": 10, "max":1000, "scale":0, "step":1}',
+        )
+        inject_dpcode(
+            mock_device,
+            "brightness_max_1",
+            1000,
+            dptype="Integer",
+            values='{"min": 10, "max":1000, "scale":0, "step":1}',
+        )
+        inject_dpcode(
+            mock_device,
+            "brightness_min_1",
+            10,
+            dptype="Integer",
+            values='{"min": 10, "max":1000, "scale":0, "step":1}',
+        )
     _inject_default_light(mock_device)
     wrapper = wrapper_type.find_dpcode(mock_device, dpcode)
 
