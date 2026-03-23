5a0859480f751ee2b981f49dcd88a94bf77fca48
epenet <6771947+epenet@users.noreply.github.com>
2026-03-23 14:44:18 +0100

    Reduce

--- src/tuya_device_handlers/device_wrapper/__init__.py
+++ src/tuya_device_handlers/device_wrapper/__init__.py
@@ -1,45 +1,11 @@
 """Tuya device wrapper."""
 
-from .alarm_control_panel import TuyaAlarmControlPanelDefinition
 from .base import DeviceWrapper
-from .binary_sensor import TuyaBinarySensorDefinition
-from .button import TuyaButtonDefinition
-from .camera import TuyaCameraDefinition
-from .climate import TuyaClimateDefinition
 from .const import DEVICE_WARNINGS
-from .cover import TuyaCoverDefinition
-from .event import TuyaEventDefinition
 from .exception import SetValueOutOfRangeError
-from .fan import TuyaFanDefinition
-from .humidifier import TuyaHumidifierDefinition
-from .light import TuyaLightDefinition
-from .number import TuyaNumberDefinition
-from .select import TuyaSelectDefinition
-from .sensor import TuyaSensorDefinition
-from .siren import TuyaSirenDefinition
-from .switch import TuyaSwitchDefinition
-from .vacuum import TuyaVacuumDefinition
-from .valve import TuyaValveDefinition
 
 __all__ = [
     "DEVICE_WARNINGS",
     "DeviceWrapper",
     "SetValueOutOfRangeError",
-    "TuyaAlarmControlPanelDefinition",
-    "TuyaBinarySensorDefinition",
-    "TuyaButtonDefinition",
-    "TuyaCameraDefinition",
-    "TuyaClimateDefinition",
-    "TuyaCoverDefinition",
-    "TuyaEventDefinition",
-    "TuyaFanDefinition",
-    "TuyaHumidifierDefinition",
-    "TuyaLightDefinition",
-    "TuyaNumberDefinition",
-    "TuyaSelectDefinition",
-    "TuyaSensorDefinition",
-    "TuyaSirenDefinition",
-    "TuyaSwitchDefinition",
-    "TuyaVacuumDefinition",
-    "TuyaValveDefinition",
 ]
