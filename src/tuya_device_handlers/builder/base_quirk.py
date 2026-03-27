8fdd3dc782a9c625b347a6394f01cb707e51c5bb
epenet <6771947+epenet@users.noreply.github.com>
2026-03-27 13:11:55 +0100

    Add entity quirks to TuyaDeviceQuirk

--- src/tuya_device_handlers/builder/base_quirk.py
+++ src/tuya_device_handlers/builder/base_quirk.py
@@ -2,100 +2,35 @@
 
 from __future__ import annotations
 
-from collections.abc import Callable
+from collections.abc import Sequence
 from dataclasses import dataclass
 import inspect
 import pathlib
 from typing import TYPE_CHECKING, Any, Self
 
-from tuya_sharing import CustomerDevice  # type: ignore[import-untyped]
-
 from tuya_device_handlers.const import DPType
-from tuya_device_handlers.device_wrapper import DeviceWrapper
-from tuya_device_handlers.helpers import (
-    TuyaClimateHVACMode,
-    TuyaCoverDeviceClass,
-    TuyaEntityCategory,
-    TuyaSensorDeviceClass,
-    TuyaSensorStateClass,
-    TuyaSwitchDeviceClass,
+from tuya_device_handlers.definition.alarm_control_panel import (
+    AlarmControlPanelQuirk,
 )
+from tuya_device_handlers.definition.binary_sensor import BinarySensorQuirk
+from tuya_device_handlers.definition.button import ButtonQuirk
+from tuya_device_handlers.definition.camera import CameraQuirk
+from tuya_device_handlers.definition.climate import ClimateQuirk
+from tuya_device_handlers.definition.cover import CoverQuirk
+from tuya_device_handlers.definition.event import EventQuirk
+from tuya_device_handlers.definition.fan import FanQuirk
+from tuya_device_handlers.definition.humidifier import HumidifierQuirk
+from tuya_device_handlers.definition.light import LightQuirk
+from tuya_device_handlers.definition.number import NumberQuirk
+from tuya_device_handlers.definition.select import SelectQuirk
+from tuya_device_handlers.definition.sensor import SensorQuirk
+from tuya_device_handlers.definition.siren import SirenQuirk
+from tuya_device_handlers.definition.switch import SwitchQuirk
+from tuya_device_handlers.definition.vacuum import VacuumQuirk
+from tuya_device_handlers.definition.valve import ValveQuirk
 
 if TYPE_CHECKING:
     from tuya_device_handlers.registry import QuirksRegistry
-
-
-type DeviceWrapperGenerator = Callable[
-    [CustomerDevice], DeviceWrapper[Any] | None
-]
-
-
-def _none_type_generator(device: CustomerDevice) -> None:
-    return None
-
-
-@dataclass
-class BaseTuyaDefinition:
-    """Definition for a Tuya entity."""
-
-    key: str
-
-    device_class: str | None = None
-    entity_category: TuyaEntityCategory | None = None
-    entity_registry_enabled_default: bool = True
-    entity_registry_visible_default: bool = True
-    icon: str | None = None
-    translation_key: str | None = None
-    translation_placeholders: dict[str, str] | None = None
-    translation_string: str | None = None
-    translation_states: dict[str, str] | None = None
-
-
-@dataclass(kw_only=True)
-class TuyaClimateDefinition(BaseTuyaDefinition):
-    """Definition for a climate entity."""
-
-    switch_only_hvac_mode: TuyaClimateHVACMode
-
-    current_temperature_dp_type: DeviceWrapperGenerator
-    target_temperature_dp_type: DeviceWrapperGenerator
-
-
-@dataclass(kw_only=True)
-class TuyaCoverDefinition(BaseTuyaDefinition):
-    """Definition for a cover entity."""
-
-    device_class: TuyaCoverDeviceClass | None = None
-
-    get_state_dp_type: DeviceWrapperGenerator
-    set_state_dp_type: DeviceWrapperGenerator
-    get_position_dp_type: DeviceWrapperGenerator
-    set_position_dp_type: DeviceWrapperGenerator
-
-
-@dataclass(kw_only=True)
-class TuyaSelectDefinition(BaseTuyaDefinition):
-    """Definition for a select entity."""
-
-    dp_type: DeviceWrapperGenerator
-
-
-@dataclass(kw_only=True)
-class TuyaSensorDefinition(BaseTuyaDefinition):
-    """Definition for a sensor entity."""
-
-    dp_type: DeviceWrapperGenerator
-    device_class: TuyaSensorDeviceClass | None = None
-    state_class: TuyaSensorStateClass | None = None
-    suggested_unit: str | None = None
-
-
-@dataclass(kw_only=True)
-class TuyaSwitchDefinition(BaseTuyaDefinition):
-    """Definition for a switch entity."""
-
-    dp_type: DeviceWrapperGenerator
-    device_class: TuyaSwitchDeviceClass | None = None
 
 
 @dataclass(kw_only=True)
@@ -113,17 +48,29 @@ class DatapointDefinition:
 class TuyaDeviceQuirk:
     """Quirk for Tuya device."""
 
+    _alarm_control_panel: list[AlarmControlPanelQuirk] | None = None
+    _binary_sensor: list[BinarySensorQuirk] | None = None
+    _button: list[ButtonQuirk] | None = None
+    _camera: list[CameraQuirk] | None = None
+    _climate: list[ClimateQuirk] | None = None
+    _cover: list[CoverQuirk] | None = None
+    _event: list[EventQuirk] | None = None
+    _fan: list[FanQuirk] | None = None
+    _humidifier: list[HumidifierQuirk] | None = None
+    _light: list[LightQuirk] | None = None
+    _number: list[NumberQuirk] | None = None
+    _select: list[SelectQuirk] | None = None
+    _sensor: list[SensorQuirk] | None = None
+    _siren: list[SirenQuirk] | None = None
+    _switch: list[SwitchQuirk] | None = None
+    _vacuum: list[VacuumQuirk] | None = None
+    _valve: list[ValveQuirk] | None = None
+
     def __init__(self) -> None:
         """Initialize the quirk."""
         self._applies_to: list[tuple[str, str]] = []
 
         self.datapoint_definitions: dict[int, DatapointDefinition] = {}
-
-        self.climate_definitions: list[TuyaClimateDefinition] = []
-        self.cover_definitions: list[TuyaCoverDefinition] = []
-        self.select_definitions: list[TuyaSelectDefinition] = []
-        self.sensor_definitions: list[TuyaSensorDefinition] = []
-        self.switch_definitions: list[TuyaSwitchDefinition] = []
 
         current_frame = inspect.currentframe()
         if TYPE_CHECKING:
@@ -189,137 +136,211 @@ class TuyaDeviceQuirk:
         )
         return self
 
-    def add_climate(
+    @property
+    def alarm_control_panel_quirks(
         self,
-        *,
-        key: str,
-        # Climate specific
-        switch_only_hvac_mode: TuyaClimateHVACMode,
-        current_temperature_dp_type: DeviceWrapperGenerator = _none_type_generator,
-        target_temperature_dp_type: DeviceWrapperGenerator = _none_type_generator,
+    ) -> Sequence[AlarmControlPanelQuirk] | None:
+        """Get alarm control panel quirks."""
+        return self._alarm_control_panel
+
+    def _add_alarm_control_panel_quirk(
+        self, quirk: AlarmControlPanelQuirk
     ) -> Self:
+        """Add alarm control panel definition."""
+        if self._alarm_control_panel is None:
+            self._alarm_control_panel = []
+        self._alarm_control_panel.append(quirk)
+        return self
+
+    @property
+    def binary_sensor_quirks(self) -> Sequence[BinarySensorQuirk] | None:
+        """Get binary sensor quirks."""
+        return self._binary_sensor
+
+    def _add_binary_sensor_quirk(self, quirk: BinarySensorQuirk) -> Self:
+        """Add binary sensor definition."""
+        if self._binary_sensor is None:
+            self._binary_sensor = []
+        self._binary_sensor.append(quirk)
+        return self
+
+    @property
+    def button_quirks(self) -> Sequence[ButtonQuirk] | None:
+        """Get button quirks."""
+        return self._button
+
+    def _add_button_quirk(self, quirk: ButtonQuirk) -> Self:
+        """Add button definition."""
+        if self._button is None:
+            self._button = []
+        self._button.append(quirk)
+
+        return self
+
+    @property
+    def camera_quirks(self) -> Sequence[CameraQuirk] | None:
+        """Get camera quirks."""
+        return self._camera
+
+    def _add_camera_quirk(self, quirk: CameraQuirk) -> Self:
+        """Add camera definition."""
+        if self._camera is None:
+            self._camera = []
+        self._camera.append(quirk)
+        return self
+
+    @property
+    def climate_quirks(self) -> Sequence[ClimateQuirk] | None:
+        """Get climate quirks."""
+        return self._climate
+
+    def _add_climate_quirk(self, quirk: ClimateQuirk) -> Self:
         """Add climate definition."""
-        self.climate_definitions.append(
-            TuyaClimateDefinition(
-                key=key,
-                switch_only_hvac_mode=switch_only_hvac_mode,
-                current_temperature_dp_type=current_temperature_dp_type,
-                target_temperature_dp_type=target_temperature_dp_type,
-            )
-        )
+        if self._climate is None:
+            self._climate = []
+        self._climate.append(quirk)
         return self
 
-    def add_cover(
-        self,
-        *,
-        key: str,
-        translation_key: str,
-        translation_string: str,
-        device_class: TuyaCoverDeviceClass | None = None,
-        # Cover specific
-        get_state_dp_type: DeviceWrapperGenerator = _none_type_generator,
-        set_state_dp_type: DeviceWrapperGenerator = _none_type_generator,
-        get_position_dp_type: DeviceWrapperGenerator = _none_type_generator,
-        set_position_dp_type: DeviceWrapperGenerator = _none_type_generator,
-    ) -> Self:
+    @property
+    def cover_quirks(self) -> Sequence[CoverQuirk] | None:
+        """Get cover quirks."""
+        return self._cover
+
+    def _add_cover_quirk(self, quirk: CoverQuirk) -> Self:
         """Add cover definition."""
-
-        self.cover_definitions.append(
-            TuyaCoverDefinition(
-                key=key,
-                translation_key=translation_key,
-                translation_string=translation_string,
-                device_class=device_class,
-                get_state_dp_type=get_state_dp_type,
-                set_state_dp_type=set_state_dp_type,
-                get_position_dp_type=get_position_dp_type,
-                set_position_dp_type=set_position_dp_type,
-            )
-        )
+        if self._cover is None:
+            self._cover = []
+        self._cover.append(quirk)
         return self
 
-    def add_select(
-        self,
-        *,
-        key: str,
-        dp_type: DeviceWrapperGenerator | None = None,
-        translation_key: str,
-        translation_string: str,
-        entity_category: TuyaEntityCategory | None = None,
-        # Select specific
-        translation_states: dict[str, str] | None = None,
-    ) -> Self:
+    @property
+    def event_quirks(self) -> Sequence[EventQuirk] | None:
+        """Get event quirks."""
+        return self._event
+
+    def _add_event_quirk(self, quirk: EventQuirk) -> Self:
+        """Add event definition."""
+        if self._event is None:
+            self._event = []
+        self._event.append(quirk)
+        return self
+
+    @property
+    def fan_quirks(self) -> Sequence[FanQuirk] | None:
+        """Get fan quirks."""
+        return self._fan
+
+    def _add_fan_quirk(self, quirk: FanQuirk) -> Self:
+        """Add fan definition."""
+        if self._fan is None:
+            self._fan = []
+        self._fan.append(quirk)
+        return self
+
+    @property
+    def humidifier_quirks(self) -> Sequence[HumidifierQuirk] | None:
+        """Get humidifier quirks."""
+        return self._humidifier
+
+    def _add_humidifier_quirk(self, quirk: HumidifierQuirk) -> Self:
+        """Add humidifier definition."""
+        if self._humidifier is None:
+            self._humidifier = []
+        self._humidifier.append(quirk)
+        return self
+
+    @property
+    def light_quirks(self) -> Sequence[LightQuirk] | None:
+        """Get light quirks."""
+        return self._light
+
+    def _add_light_quirk(self, quirk: LightQuirk) -> Self:
+        """Add light definition."""
+        if self._light is None:
+            self._light = []
+        self._light.append(quirk)
+        return self
+
+    @property
+    def number_quirks(self) -> Sequence[NumberQuirk] | None:
+        """Get number quirks."""
+        return self._number
+
+    def _add_number_quirk(self, quirk: NumberQuirk) -> Self:
+        """Add number definition."""
+        if self._number is None:
+            self._number = []
+        self._number.append(quirk)
+        return self
+
+    @property
+    def select_quirks(self) -> Sequence[SelectQuirk] | None:
+        """Get select quirks."""
+        return self._select
+
+    def _add_select_quirk(self, quirk: SelectQuirk) -> Self:
         """Add select definition."""
-        if dp_type is None:
-            raise NotImplementedError
-        self.select_definitions.append(
-            TuyaSelectDefinition(
-                key=key,
-                dp_type=dp_type,
-                translation_key=translation_key,
-                translation_string=translation_string,
-                entity_category=entity_category,
-                translation_states=translation_states,
-            )
-        )
+        if self._select is None:
+            self._select = []
+        self._select.append(quirk)
         return self
 
-    def add_sensor(
-        self,
-        *,
-        key: str,
-        dp_type: DeviceWrapperGenerator | None = None,
-        translation_key: str | None = None,
-        translation_string: str | None = None,
-        device_class: TuyaSensorDeviceClass | None = None,
-        entity_category: TuyaEntityCategory | None = None,
-        entity_registry_enabled_default: bool = False,
-        # Sensor specific
-        state_class: TuyaSensorStateClass | None = None,
-        suggested_unit: str | None = None,
-    ) -> Self:
+    @property
+    def sensor_quirks(self) -> Sequence[SensorQuirk] | None:
+        """Get sensor quirks."""
+        return self._sensor
+
+    def _add_sensor_quirk(self, quirk: SensorQuirk) -> Self:
         """Add sensor definition."""
-        if dp_type is None:
-            raise NotImplementedError
-        self.sensor_definitions.append(
-            TuyaSensorDefinition(
-                key=key,
-                dp_type=dp_type,
-                translation_key=translation_key,
-                translation_string=translation_string,
-                device_class=device_class,
-                entity_category=entity_category,
-                entity_registry_enabled_default=entity_registry_enabled_default,
-                state_class=state_class,
-                suggested_unit=suggested_unit,
-            )
-        )
+        if self._sensor is None:
+            self._sensor = []
+        self._sensor.append(quirk)
         return self
 
-    def add_switch(
-        self,
-        *,
-        key: str,
-        dp_type: DeviceWrapperGenerator | None = None,
-        translation_key: str | None = None,
-        translation_string: str | None = None,
-        translation_placeholders: dict[str, str] | None = None,
-        device_class: TuyaSwitchDeviceClass | None = None,
-        entity_category: TuyaEntityCategory | None = None,
-        # Switch specific
-    ) -> Self:
+    @property
+    def siren_quirks(self) -> Sequence[SirenQuirk] | None:
+        """Get siren quirks."""
+        return self._siren
+
+    def _add_siren_quirk(self, quirk: SirenQuirk) -> Self:
+        """Add siren definition."""
+        if self._siren is None:
+            self._siren = []
+        self._siren.append(quirk)
+        return self
+
+    @property
+    def switch_quirks(self) -> Sequence[SwitchQuirk] | None:
+        """Get switch quirks."""
+        return self._switch
+
+    def _add_switch_quirk(self, quirk: SwitchQuirk) -> Self:
         """Add switch definition."""
-        if dp_type is None:
-            raise NotImplementedError
-        self.switch_definitions.append(
-            TuyaSwitchDefinition(
-                key=key,
-                dp_type=dp_type,
-                translation_key=translation_key,
-                translation_string=translation_string,
-                translation_placeholders=translation_placeholders,
-                device_class=device_class,
-                entity_category=entity_category,
-            )
-        )
+        if self._switch is None:
+            self._switch = []
+        self._switch.append(quirk)
+        return self
+
+    @property
+    def vacuum_quirks(self) -> Sequence[VacuumQuirk] | None:
+        """Get vacuum quirks."""
+        return self._vacuum
+
+    def _add_vacuum_quirk(self, quirk: VacuumQuirk) -> Self:
+        """Add vacuum definition."""
+        if self._vacuum is None:
+            self._vacuum = []
+        self._vacuum.append(quirk)
+        return self
+
+    @property
+    def valve_quirks(self) -> Sequence[ValveQuirk] | None:
+        """Get valve quirks."""
+        return self._valve
+
+    def _add_valve_quirk(self, quirk: ValveQuirk) -> Self:
+        """Add valve definition."""
+        if self._valve is None:
+            self._valve = []
+        self._valve.append(quirk)
         return self
