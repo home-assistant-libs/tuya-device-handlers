cfa3ebc9cf7a4cec7f9922c82d9cafa1486cfc6e
epenet <6771947+epenet@users.noreply.github.com>
2026-03-27 11:04:57 +0100

    Add camera

--- src/tuya_device_handlers/definition/camera.py
+++ src/tuya_device_handlers/definition/camera.py
@@ -1,17 +1,29 @@
 """Tuya camera definition."""
 
+from collections.abc import Callable
 from dataclasses import dataclass
 
 from tuya_sharing import CustomerDevice  # type: ignore[import-untyped]
 
 from ..device_wrapper import DeviceWrapper
 from ..device_wrapper.common import DPCodeBooleanWrapper
+from .base import BaseEntityQuirk
 
 
 @dataclass
 class TuyaCameraDefinition:
     motion_detection_switch: DeviceWrapper[bool] | None
     recording_status: DeviceWrapper[bool] | None
+
+
+@dataclass(kw_only=True)
+class CameraQuirk(BaseEntityQuirk):
+    """Quirk for a camera entity."""
+
+    definition_fn: Callable[
+        [CustomerDevice],
+        TuyaCameraDefinition | None,
+    ]
 
 
 def get_default_definition(device: CustomerDevice) -> TuyaCameraDefinition:
