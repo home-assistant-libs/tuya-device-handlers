3e2626de9aef65dab1d0fec40b80e980766a00c0
epenet <6771947+epenet@users.noreply.github.com>
2026-03-27 11:36:14 +0100

    Update BaseEntityQuirk

--- src/tuya_device_handlers/definition/base.py
+++ src/tuya_device_handlers/definition/base.py
@@ -14,5 +14,6 @@ class BaseEntityQuirk:
     device_class: str | None = None
     entity_category: TuyaEntityCategory | None = None
     entity_registry_enabled_default: bool = True
+    icon: str | None = None
     translation_key: str | None = None
     translation_placeholders: dict[str, str] | None = None
