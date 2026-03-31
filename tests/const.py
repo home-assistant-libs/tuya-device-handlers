14564b21812af15f54869a0286f279c8274abbbb
epenet <6771947+epenet@users.noreply.github.com>
2026-03-31 08:18:08 +0200

    Cleanup test fixture exclusion

--- tests/const.py
+++ tests/const.py
@@ -2,26 +2,4 @@
 
 import glob
 
-DEVICE_FIXTURES = [
-    fixture_filename
-    for fixture_filename in glob.glob(
-        "*.json", root_dir="tests/fixtures/devices"
-    )
-    if fixture_filename
-    not in {
-        "cl_zah67ekd.json",
-        "cs_zibqa9dutqyaxym2.json",
-        "cwwsq_wfkzyy0evslzsmoi.json",
-        "cz_guitoc9iylae4axs.json",
-        "cz_PGEkBctAbtzKOZng.json",
-        "dj_mki13ie507rlry4r.json",
-        "kt_5wnlzekkstwcdsvm.json",
-        "mal_gyitctrjj1kefxp2.json",
-        "sd_i6hyjg3af7doaswm.json",
-        "sd_lr33znaodtyarrrz.json",
-        "sfkzq_ed7frwissyqrejic.json",
-        "sp_rudejjigkywujjvs.json",
-        "sp_sdd5f5f2dl5wydjf.json",
-        "wk_B0eP8qYAdpUo4yR9.json",
-    }
-]
+DEVICE_FIXTURES = glob.glob("*.json", root_dir="tests/fixtures/devices")
