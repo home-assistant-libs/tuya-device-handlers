f9b22be0fcf2e4ace196ce59178a080b7d03aa13
epenet <6771947+epenet@users.noreply.github.com>
2026-04-10 14:22:17 +0200

    Refactor climate get_default_definition

--- src/tuya_device_handlers/definition/climate.py
+++ src/tuya_device_handlers/definition/climate.py
@@ -70,40 +70,19 @@ def _get_temperature_wrapper(
     )
 
 
-def _get_temperature_wrappers(
-    device: CustomerDevice, system_temperature_unit: TuyaUnitOfTemperature
+def get_temperature_wrappers(
+    *,
+    temp_current: DPCodeIntegerWrapper | None,
+    temp_current_f: DPCodeIntegerWrapper | None,
+    temp_set: DPCodeIntegerWrapper | None,
+    temp_set_f: DPCodeIntegerWrapper | None,
+    system_temperature_unit: TuyaUnitOfTemperature,
 ) -> tuple[
     DPCodeIntegerWrapper | None,
     DPCodeIntegerWrapper | None,
     TuyaUnitOfTemperature,
 ]:
     """Get temperature wrappers for current and set temperatures."""
-    # Get all possible temperature dpcodes
-    temp_current = DPCodeIntegerWrapper.find_dpcode(
-        device, ("temp_current", "upper_temp")
-    )
-    temp_current_f = DPCodeIntegerWrapper.find_dpcode(
-        device, ("temp_current_f", "upper_temp_f")
-    )
-    temp_set = DPCodeIntegerWrapper.find_dpcode(
-        device, "temp_set", prefer_function=True
-    )
-    temp_set_f = DPCodeIntegerWrapper.find_dpcode(
-        device, "temp_set_f", prefer_function=True
-    )
-
-    # If there is a temp unit convert dpcode, override empty units
-    if (
-        temp_unit_convert := DPCodeEnumWrapper.find_dpcode(
-            device, "temp_unit_convert"
-        )
-    ) is not None:
-        for wrapper in (temp_current, temp_current_f, temp_set, temp_set_f):
-            if wrapper is not None and not wrapper.type_information.unit:
-                wrapper.type_information.unit = (
-                    temp_unit_convert.read_device_status(device)
-                )
-
     # Get wrappers for celsius and fahrenheit
     # We need to check the unit of measurement
     current_celsius = _get_temperature_wrapper(
@@ -154,17 +133,60 @@ def _get_temperature_wrappers(
     )
 
 
+def _get_default_temperature_wrappers(
+    device: CustomerDevice, system_temperature_unit: TuyaUnitOfTemperature
+) -> tuple[
+    DPCodeIntegerWrapper | None,
+    DPCodeIntegerWrapper | None,
+    TuyaUnitOfTemperature,
+]:
+    """Get temperature wrappers for current and set temperatures."""
+    # Get all possible temperature dpcodes
+    temp_current = DPCodeIntegerWrapper.find_dpcode(
+        device, ("temp_current", "upper_temp")
+    )
+    temp_current_f = DPCodeIntegerWrapper.find_dpcode(
+        device, ("temp_current_f", "upper_temp_f")
+    )
+    temp_set = DPCodeIntegerWrapper.find_dpcode(
+        device, "temp_set", prefer_function=True
+    )
+    temp_set_f = DPCodeIntegerWrapper.find_dpcode(
+        device, "temp_set_f", prefer_function=True
+    )
+
+    # If there is a temp unit convert dpcode, override empty units
+    if (
+        temp_unit_convert := DPCodeEnumWrapper.find_dpcode(
+            device, "temp_unit_convert"
+        )
+    ) is not None:
+        for wrapper in (temp_current, temp_current_f, temp_set, temp_set_f):
+            if wrapper is not None and not wrapper.type_information.unit:
+                wrapper.type_information.unit = (
+                    temp_unit_convert.read_device_status(device)
+                )
+
+    return get_temperature_wrappers(
+        temp_current=temp_current,
+        temp_current_f=temp_current_f,
+        temp_set=temp_set,
+        temp_set_f=temp_set_f,
+        system_temperature_unit=system_temperature_unit,
+    )
+
+
 def get_default_definition(
     device: CustomerDevice, system_temperature_unit: TuyaUnitOfTemperature
 ) -> ClimateDefinition:
-    temperature_wrappers = _get_temperature_wrappers(
-        device, system_temperature_unit
+    current_temperature_wrapper, set_temperature_wrapper, temperature_unit = (
+        _get_default_temperature_wrappers(device, system_temperature_unit)
     )
     return ClimateDefinition(
         current_humidity_wrapper=DPCodeRoundedIntegerWrapper.find_dpcode(
             device, "humidity_current"
         ),
-        current_temperature_wrapper=temperature_wrappers[0],
+        current_temperature_wrapper=current_temperature_wrapper,
         fan_mode_wrapper=DPCodeEnumWrapper.find_dpcode(
             device,
             ("fan_speed_enum", "level", "windspeed"),
@@ -176,7 +198,7 @@ def get_default_definition(
         preset_wrapper=DefaultPresetModeWrapper.find_dpcode(
             device, "mode", prefer_function=True
         ),
-        set_temperature_wrapper=temperature_wrappers[1],
+        set_temperature_wrapper=set_temperature_wrapper,
         swing_wrapper=SwingModeCompositeWrapper.find_dpcode(device),
         switch_wrapper=DPCodeBooleanWrapper.find_dpcode(
             device, "switch", prefer_function=True
@@ -184,5 +206,5 @@ def get_default_definition(
         target_humidity_wrapper=DPCodeRoundedIntegerWrapper.find_dpcode(
             device, "humidity_set", prefer_function=True
         ),
-        temperature_unit=temperature_wrappers[2],
+        temperature_unit=temperature_unit,
     )
