8d55ec0c5fccb9c6ca5ccd10d239be5cc3a466eb
epenet <6771947+epenet@users.noreply.github.com>
2026-04-23 17:18:31 +0200

    Fix diagnostic test name and docstring

--- tests/helpers/test_diagnostics.py
+++ tests/helpers/test_diagnostics.py
@@ -7,7 +7,7 @@ from tuya_device_handlers.helpers.diagnostics import customer_device_as_dict
 from .. import create_device
 
 
-def test_get_default_definition_fails(snapshot: SnapshotAssertion) -> None:
-    """Test get_default_definition"""
+def test_customer_device_as_dict(snapshot: SnapshotAssertion) -> None:
+    """Test customer_device_as_dict"""
     device = create_device("sfkzq_ed7frwissyqrejic.json")
     assert customer_device_as_dict(device) == snapshot
