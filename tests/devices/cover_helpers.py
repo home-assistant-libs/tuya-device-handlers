0d63d315551e673c2010a3413873a7cdf0090810
epenet <6771947+epenet@users.noreply.github.com>
2026-04-24 10:06:27 +0200

    Improve cover helpers

--- tests/devices/cover_helpers.py
+++ tests/devices/cover_helpers.py
@@ -2,7 +2,10 @@
 
 from tuya_sharing import CustomerDevice
 
-from tuya_device_handlers.definition import cover as cover_definition
+from tuya_device_handlers.definition.cover import (
+    CoverDefinition,
+    get_default_definition,
+)
 from tuya_device_handlers.device_wrapper.cover import (
     CoverInstructionSpecialEnumWrapper,
 )
@@ -10,36 +13,36 @@ from tuya_device_handlers.device_wrapper.cover import (
 
 def _get_cl_cover_default_definitions(
     device: CustomerDevice,
-) -> list[cover_definition.CoverDefinition | None]:
+) -> list[CoverDefinition | None]:
     """Get the default cover definition for a CL device."""
     return [
-        cover_definition.get_default_definition(
+        get_default_definition(
             device,
             instruction_dpcode="control",
             current_state_dpcode=("situation_set", "control"),
             current_position_dpcode=("percent_state", "percent_control"),
             set_position_dpcode="percent_control",
         ),
-        cover_definition.get_default_definition(
+        get_default_definition(
             device,
             instruction_dpcode="control_2",
             current_position_dpcode="percent_state_2",
             set_position_dpcode="percent_control_2",
         ),
-        cover_definition.get_default_definition(
+        get_default_definition(
             device,
             instruction_dpcode="control_3",
             current_position_dpcode="percent_state_3",
             set_position_dpcode="percent_control_3",
         ),
-        cover_definition.get_default_definition(
+        get_default_definition(
             device,
             instruction_dpcode="mach_operate",
             current_position_dpcode="position",
             set_position_dpcode="position",
             instruction_wrapper=CoverInstructionSpecialEnumWrapper,
         ),
-        cover_definition.get_default_definition(
+        get_default_definition(
             device,
             instruction_dpcode="switch_1",
             current_position_dpcode="percent_control",
@@ -50,9 +53,9 @@ def _get_cl_cover_default_definitions(
 
 def get_cover_default_definitions(
     device: CustomerDevice,
-) -> list[cover_definition.CoverDefinition]:
+) -> list[CoverDefinition]:
     """Get the default cover definition for a device."""
-    values: list[cover_definition.CoverDefinition | None] = []
+    values: list[CoverDefinition | None] = []
     if device.category == "cl":
         values.extend(_get_cl_cover_default_definitions(device))
     return [definition for definition in values if definition]
