1afd7711eb364394bcb380bc116a1b2968c3827f
epenet <6771947+epenet@users.noreply.github.com>
2026-05-18 12:22:46 +0200

    Add original_category to diagnostics

--- tests/helpers/test_diagnostics.py
+++ tests/helpers/test_diagnostics.py
@@ -4,9 +4,31 @@ from syrupy.assertion import SnapshotAssertion
 
 from tests import create_device
 from tuya_device_handlers.helpers.diagnostics import customer_device_as_dict
+from tuya_device_handlers.registry import QuirksRegistry
 
 
 def test_customer_device_as_dict(snapshot: SnapshotAssertion) -> None:
     """Test customer_device_as_dict."""
     device = create_device("sfkzq_ed7frwissyqrejic.json")
-    assert customer_device_as_dict(device) == snapshot
+    data = customer_device_as_dict(device)
+    assert data == snapshot
+    assert "original_category" not in data
+    assert "original_function" not in data
+    assert "original_local_strategy" not in data
+    assert "original_status_range" not in data
+
+
+def test_customer_device_with_quirk_as_dict(
+    filled_quirks_registry: QuirksRegistry, snapshot: SnapshotAssertion
+) -> None:
+    """Test customer_device_as_dict."""
+    device = create_device("tdq_x3o8epevyeo3z3oa.json")
+    data = customer_device_as_dict(device)
+
+    filled_quirks_registry.initialise_device_quirk(device)
+
+    assert data == snapshot
+    assert "original_category" in data
+    assert "original_function" in data
+    assert "original_local_strategy" in data
+    assert "original_status_range" in data
