1397ea5007ee023fbe17646eabdcad07e2dd2dad
epenet <6771947+epenet@users.noreply.github.com>
2026-06-05 06:46:42 +0000

    Only map "mode" enums

--- src/tuya_device_handlers/devices/wk/cpmgn2cf.py
+++ src/tuya_device_handlers/devices/wk/cpmgn2cf.py
@@ -14,15 +14,19 @@ from tuya_device_handlers.const import DPMode
 (
     DeviceQuirk()
     .applies_to(product_id="cpmgn2cf")
-    .add_dpid_integer(
-        dpid=109,
-        dpcode="valve",
-        dpmode=DPMode.READ,
-        unit="%",
-        min=0,
-        max=100,
-        scale=0,
-        step=1,
+    .add_dpid_enum(
+        dpid=4,
+        dpcode="mode",
+        dpmode=DPMode.READ | DPMode.WRITE,
+        enum_range=[
+            "holiday",
+            "auto",
+            "manual",
+            "comfort",
+            "eco",
+            "BOOST",
+            "temp_auto",
+        ],
     )
     .register(TUYA_QUIRKS_REGISTRY)
 )
--- tests/devices/wk/test_cpmgn2cf.py
+++ tests/devices/wk/test_cpmgn2cf.py
@@ -1,17 +1,42 @@
 """Test device-level quirk initialisation."""
 
 from tests import create_device
-from tuya_device_handlers.definition.sensor import get_default_definition
+from tuya_device_handlers.definition.climate import get_default_definition
+from tuya_device_handlers.helpers.homeassistant import TuyaUnitOfTemperature
 from tuya_device_handlers.registry import QuirksRegistry
 
 
-def test_valve_sensor_wk(
+def test_quirk(
     filled_quirks_registry: QuirksRegistry,
 ) -> None:
-    """WK quirk registers the valve datapoint as a sensor."""
+    """Test quirk."""
     device = create_device("wk_cpmgn2cf.json")
-    assert get_default_definition(device, "valve") is None
+
+    definition = get_default_definition(device, TuyaUnitOfTemperature.CELSIUS)
+    assert definition is not None
+    assert definition.hvac_mode_wrapper is not None
+    assert definition.hvac_mode_wrapper.options == []
+    assert definition.preset_wrapper is not None
+    assert definition.preset_wrapper.options == [
+        "holiday",
+        "auto",
+        "manual",
+        "eco",
+    ]
 
     filled_quirks_registry.initialise_device_quirk(device)
 
-    assert get_default_definition(device, "valve") is not None
+    definition = get_default_definition(device, TuyaUnitOfTemperature.CELSIUS)
+    assert definition is not None
+    assert definition.hvac_mode_wrapper is not None
+    assert definition.hvac_mode_wrapper.options == []
+    assert definition.preset_wrapper is not None
+    assert definition.preset_wrapper.options == [
+        "holiday",
+        "auto",
+        "manual",
+        "comfort",
+        "eco",
+        "BOOST",
+        "temp_auto",
+    ]
